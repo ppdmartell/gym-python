1847bd8a3e134aceee0b059c6580b4f19ae6a454
ppdmartell <ppdmartell@gmail.com>
2025-07-07 16:52:42 -0300

    Falsey and truthy values and bool() method.

---
+++ examples/truthy_falsey.py
@@ -0,0 +1,11 @@
+var = input("Enter a string value, or not: ")
+
+if(var):
+    print("Than you for entering something.")
+else:
+    print("You didn't enter something.")
+
+
+print(bool(0))
+print(bool(""))
+
